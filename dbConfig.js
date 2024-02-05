44476624eb18807b0b51ca5c71df9d7965ee4616
Madan raj <125730836+Madanraj0519@users.noreply.github.com>
2024-02-05 10:43:05 +0530

    "success"

--- dbConfig.js
+++ dbConfig.js
@@ -2,7 +2,7 @@ const { default: mongoose } = require("mongoose");
 
 async function connectDatabase(){
     try{
-        const response = await mongoose.connect("mongodb://127.0.0.1:27017/bookings");
+        const response = await mongoose.connect(`mongodb://127.0.0.1:27017/${process.env.MONGODB_URL}`);
         if(response.connections.length > 0){
             console.log('Database connection successfully established');
         }else{
