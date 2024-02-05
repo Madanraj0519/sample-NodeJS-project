31582a4ac3cc40911f32a50a7c96e21c991b8710
Madan raj <125730836+Madanraj0519@users.noreply.github.com>
2024-02-06 01:59:21 +0530

    "Added Atlas url"

--- dbConfig.js
+++ dbConfig.js
@@ -3,9 +3,10 @@ require('dotenv').config()
 
 async function connectDatabase(){
     try{
-        const response = await mongoose.connect(`mongodb://127.0.0.1:27017/${process.env.MONGODB_URL}`,{
-          
-        });
+        const response = await mongoose.connect(`${ 
+            process.env.Node_Env !== 'development' ? 
+            process.env.MONGODB_LOCAL_URL : process.env.MONGODB_ATLAS_URL}
+            `);
         if(response.connections.length > 0){
             console.log('Database connection successfully established');
         }else{
