9d4141c74208d6c6056d844e7d326d96c5c62cf0
Madan raj <125730836+Madanraj0519@users.noreply.github.com>
2024-02-05 11:13:21 +0530

    "changes i mongoDB url"

--- dbConfig.js
+++ dbConfig.js
@@ -3,7 +3,7 @@ require('dotenv').config()
 
 async function connectDatabase(){
     try{
-        const response = await mongoose.connect(`mongodb://127.0.0.1:27017/${process.env.MONGODB_URL}`);
+        const response = await mongoose.connect(`mongodb://localhost:27017/${process.env.MONGODB_URL}`);
         if(response.connections.length > 0){
             console.log('Database connection successfully established');
         }else{
